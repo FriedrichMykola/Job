0cde0e6d1b1029b9416611867006f1e3ecf1bec4
Mykola <kuzanmykola@gmail.com>
2019-04-16 00:16:06 +0300

    no message

---
+++ KotlinNative/app/src/main/java/com/example/business/friedrich/kuzan/kotlinnative/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.business.friedrich.kuzan.kotlinnative
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
